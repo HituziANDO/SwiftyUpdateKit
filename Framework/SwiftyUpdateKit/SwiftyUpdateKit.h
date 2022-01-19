2ced7079ee76c79014a5a3e9526b979a626ea669
hituzi ando <hhh.ando@gmail.com>
2022-01-19 17:48:58 +0900

    Fix typo

--- Framework/SwiftyUpdateKit/SwiftyUpdateKit.h
+++ Framework/SwiftyUpdateKit/SwiftyUpdateKit.h
@@ -12,6 +12,6 @@ FOUNDATION_EXPORT double SwiftyUpdateKitVersionNumber;
 //! Project version string for SwiftyUpdateKit.
 FOUNDATION_EXPORT const unsigned char SwiftyUpdateKitVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <CLYRSwiftToolKit/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyUpdateKit/PublicHeader.h>
 
 
