a24aa0b8b3b7808ad6559dbd08a9e13801f6c57b
Hituzi Ando <hhh.ando@gmail.com>
2024-04-16 17:49:33 +0900

    ver.1.4.0

--- Framework/SwiftyUpdateKit.xcframework/ios-arm64/SwiftyUpdateKit.framework/Headers/SwiftyUpdateKit.h
+++ Framework/SwiftyUpdateKit.xcframework/ios-arm64/SwiftyUpdateKit.framework/Headers/SwiftyUpdateKit.h
@@ -6,10 +6,10 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for SwiftyUpdateKit.
+// ! Project version number for SwiftyUpdateKit.
 FOUNDATION_EXPORT double SwiftyUpdateKitVersionNumber;
 
-//! Project version string for SwiftyUpdateKit.
+// ! Project version string for SwiftyUpdateKit.
 FOUNDATION_EXPORT const unsigned char SwiftyUpdateKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SwiftyUpdateKit/PublicHeader.h>
